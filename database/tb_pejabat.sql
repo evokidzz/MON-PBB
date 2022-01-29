04ecc8925de021c5a6cfa176daf7aaff7b29035c
evokidzz <adikolo13@gmail.com>
2022-01-29 11:26:02 +0800

    update db

--- database/tb_pejabat.sql
+++ database/tb_pejabat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2022 at 07:32 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.26
+-- Generation Time: Jan 28, 2022 at 05:17 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,36 +18,39 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `adm_desa`
+-- Database: `mon_pbb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_penduduk`
+-- Table structure for table `tb_pejabat`
 --
 
-CREATE TABLE `tb_penduduk` (
-  `nik` int(16) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `tmpt_lahir` varchar(100) NOT NULL,
-  `tgl_lahir` date NOT NULL,
-  `jenis_kelamin` enum('L','P','','') NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `agama` varchar(50) NOT NULL,
-  `status` enum('Belum Menikah','Menikah','Duda/Janda','') NOT NULL,
-  `pekerjaan` varchar(150) NOT NULL
+CREATE TABLE `tb_pejabat` (
+  `id_pejabat` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jabatan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pejabat`
+--
+
+INSERT INTO `tb_pejabat` (`id_pejabat`, `nama`, `jabatan`) VALUES
+(0, 'SUPARMAN', 'PEMBAKAL'),
+(1, 'LUTFI WARDANI', 'SEKDES');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_penduduk`
+-- Indexes for table `tb_pejabat`
 --
-ALTER TABLE `tb_penduduk`
-  ADD PRIMARY KEY (`nik`);
+ALTER TABLE `tb_pejabat`
+  ADD PRIMARY KEY (`id_pejabat`);
+ALTER TABLE `tb_pejabat` ADD FULLTEXT KEY `nama` (`nama`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
