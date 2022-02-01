d9732b17aaf36f469c720dc919de3ff67b573be6
evokidzz <adikolo13@gmail.com>
2022-02-01 20:40:24 +0800

    Update data_pejabat.php

--- admin/profil/data_pejabat.php
+++ admin/profil/data_pejabat.php
@@ -1,18 +1,26 @@
 <div class="card card-info">
 	<div class="card-header">
 		<h3 class="card-title">
-			<i class="fa fa-table"></i> Nama Pejabat Desa
+			<i class="fa fa-table"></i> Nama Pejabat BAPENDA
 		</h3>
 	</div>
 	<!-- /.card-header -->
 	<div class="card-body">
 		<div class="table-responsive">
+			<div>
+				<a href="?page=add-pejabat" class="btn btn-primary">
+					<i class="fa fa-edit"></i> Tambah Data</a>
+				<a href="admin\wp\wp\cetak_lap_wp.php" target="_blank" title="Cetak" class="btn btn-success"><i class="fa fa-print">
+					</i> Cetak Data</a>
+			</div>
+			<br>
 			<table id="" class="table table-bordered table-striped">
 				<thead>
 					<tr>
 						<th>Nama</th>
 						<th>Jabatan</th>
-						<th>Kelola</th>
+						<!-- <th>NIP</th> -->
+						<th class="text-center">Kelola</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -30,10 +38,15 @@
 							<td>
 								<?php echo $data['jabatan']; ?>
 							</td>
-							<td>
+							<!-- <td>
+								<?php echo $data['nip']; ?>
+							</td> -->
+							<td class="text-center">
 								<a href="?page=edit-pejabat&kode=<?php echo $data['id_pejabat']; ?>" title="Ubah" class="btn btn-success btn-sm">
 									<i class="fa fa-wrench"></i>
 								</a>
+								<a href="?page=del-pejabat&kode=<?php echo $data['nik']; ?>" onclick="return confirm('Apakah anda yakin hapus data ini ?')" title="Hapus" class="btn btn-danger btn-sm">
+									<i class="fa fa-trash"></i>
 							</td>
 						</tr>
 
