e3d7d9869fd5d74667174a63ec3cfbd6f146b070
evokidzz <adikolo13@gmail.com>
2022-02-06 14:47:18 +0800

    Create admin.php

---
+++ home/admin.php
@@ -0,0 +1,58 @@
+<div class="card card-dark">
+    <div class="card-header">
+        <h3 class="card-title">
+            <i class="fa fa-table"></i> Daftar Pejabat BAPENDA
+        </h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+        <div class="table-responsive">
+            <div>
+                <a href="admin\cetak\pejabat\cetak_lap_pejabat.php" target="_blank" title="Cetak" class="btn btn-success"><i class="fa fa-print">
+                    </i> Cetak Data</a>
+            </div>
+            <br>
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th class="text-center">Nama</th>
+                        <th class="text-center">Jabatan</th>
+                        <th class="text-center">Kelola</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                    $no = 1;
+                    $sql = $koneksi->query("select * from tb_pejabat order by id_pejabat asc");
+                    while ($data = $sql->fetch_assoc()) {
+                    ?>
+
+                        <tr>
+                            <td><?php echo $no++; ?></td>
+                            <td>
+                                <?php echo $data['nama']; ?>
+                            </td>
+                            <td>
+                                <?php echo $data['jabatan']; ?>
+                            </td>
+
+                            <td class="text-center">
+                                <a href="?page=edit-pejabat&kode=<?php echo $data['id_pejabat']; ?>" title="Ubah" class="btn btn-success btn-sm">
+                                    <i class="fa fa-wrench"></i>
+                                </a>
+                                <a href="?page=del-pejabat&kode=<?php echo $data['nik']; ?>" onclick="return confirm('Apakah anda yakin hapus data ini ?')" title="Hapus" class="btn btn-danger btn-sm">
+                                    <i class="fa fa-trash"></i>
+                            </td>
+                        </tr>
+
+                    <?php
+                    }
+                    ?>
+                </tbody>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+    <!-- /.card-body -->
