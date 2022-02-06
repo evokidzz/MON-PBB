088bbf121f6abed3510f591877182da965a213a0
evokidzz <adikolo13@gmail.com>
2022-02-06 16:36:34 +0800

    Update edit_mutasi.php

--- admin/surat/mutasi/edit_mutasi.php
+++ admin/surat/mutasi/edit_mutasi.php
@@ -94,7 +94,7 @@ if (isset($_GET['kode'])) {
         </div>
         <div class="card-footer">
             <input type="submit" name="Ubah" value="Ubah" class="btn btn-info">
-            <a href="?page=data-baru" title="Kembali" class="btn btn-secondary">Batal</a>
+            <a href="?page=data-mutasi" title="Kembali" class="btn btn-secondary">Batal</a>
         </div>
     </form>
 </div>
